fa3bd142207c33bc7f869f4f40f8122900a21647
SCREENSABER <61020218+SCREENSABER@users.noreply.github.com>
2021-02-14 11:31:09 -0600

    answered var, let and const problems

---
+++ app.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+PI = 42;
+
+// difference between var and let is let can be reassigned but not redefined like var.
+// difference between var and const is const cannot be reassigned or redefined unlike var.
+//diffence between let and const is let can be reassined while const cannot.
+//hoisting is when variables are used before they are declared.
